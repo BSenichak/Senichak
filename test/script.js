d655516f28bb7398c572bca96bd87e543dcea3f7
BSenichak <bsenk77@gmail.com>
2022-02-02 18:00:25 +0200

    first load

---
+++ test/script.js
@@ -0,0 +1,7 @@
+
+
+function myfunc() {
+    
+    document.getElementsByClassName("button")[0].innerHTML = "Хахахахах Лох ти шо повірив?";
+    document.getElementById("trololo").style.display = "block"
+}
