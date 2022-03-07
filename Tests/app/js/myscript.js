ffec7a061a00d0a88baaebdc6530f56eb9e83572
BSenichak <bsenk77@gmail.com>
2022-03-07 13:34:22 +0200

    pusto

---
+++ Tests/app/js/myscript.js
@@ -0,0 +1,6 @@
+document.write("<h1>Hello world</h1>");
+let a;
+let b;
+if(a != undefined && b != undefined){
+    console.log(a + b);
+}
--- Tests/test/script.js
+++
@@ -1,7 +0,0 @@
-
-
-function myfunc() {
-    
-    document.getElementsByClassName("button")[0].innerHTML = "Хахахахах Лох ти шо повірив?";
-    document.getElementById("trololo").style.display = "block"
-}
