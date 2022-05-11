dcb10d86b2fd2cff88edfcd302699261368c1d34
BSenichak <bsenk77@gmail.com>
2022-05-11 08:35:51 +0300

    summary

--- nodejs/server/l1/simpleserv.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
- 
-//http.createServer().listen(3000);
- 
-http.createServer(function(request, response){
-     
-    response.end("Hello worlds!");
-}).listen(3000);
--- nodejs/start/lesson 9/app.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
- 
-let readableStream = fs.createReadStream("hello.txt", "utf8");
- 
-let writeableStream = fs.createWriteStream("some2.txt");
- 
-readableStream.pipe(writeableStream);
